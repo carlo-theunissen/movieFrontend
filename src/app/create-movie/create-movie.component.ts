7842fa0065740f22b9542d39198131238c559001
Carlo THeunissen <carlotheunissen96@gmail.com>
2018-12-19 14:09:43 +0100

    checkboxes

--- src/app/create-movie/create-movie.component.ts
+++ src/app/create-movie/create-movie.component.ts
@@ -12,14 +12,65 @@ export class CreateMovieComponent implements OnInit {
   public name : string;
   public description : string;
   public duration : number;
-  public actors : string;
-  public genres : string;
+  public actorList : any;
+  public genresList : any;
   public bgImage : string;
   constructor(private http : HttpClient) { }
 
   ngOnInit() {
+    this.loadActors();
+    this.loadGenres();
   }
 
+  private loadActors() : void{
+    //port
+    let url = "http://"+location.hostname;
+    if(location.port !== ""){
+      url += ":8090" + "";
+    }
+
+    this.http.get(url+"/api/actors").subscribe(
+      (val) => {
+        this.actorList = val;
+
+      },
+      response => {
+        //setTimeout(() => this.populateWithNewData(), 1000);
+      },
+      () => {
+        //The POST observable is now completed
+      });
+  }
+
+  private loadGenres() : void{
+    //port
+    let url = "http://"+location.hostname;
+    if(location.port !== ""){
+      url += ":8090" + "";
+    }
+
+    this.http.get(url+"/api/genres").subscribe(
+      (val) => {
+        this.genresList = val;
+
+      },
+      response => {
+        //setTimeout(() => this.populateWithNewData(), 1000);
+      },
+      () => {
+        //The POST observable is now completed
+      });
+  }
+  private getArray(process : any) : number[]{
+    let result = [];
+    for (let obj of process) {
+      const work = <any> obj;
+      if(work.checked){
+        result.push(work.id);
+      }
+    }
+    return result;
+  }
   public handleSubmit() : void{
     const httpOptions = {
       headers: new HttpHeaders({
@@ -31,10 +82,13 @@ export class CreateMovieComponent implements OnInit {
       "Name": this.name,
       "Description" : this.description,
       "Duration" : this.duration,
-      "Actors" : this.actors.split(','),
-      "Genres" : this.genres.split(','),
+      "Actors" : this.getArray(this.actorList),
+      "Genres" : this.getArray(this.genresList),
       "BgImage" : this.bgImage
     };
+
+    console.log(this.actorList);
+    console.log(this.genresList);
 
     console.log(JSON.stringify(data));
     this.state = "loading";
@@ -44,10 +98,12 @@ export class CreateMovieComponent implements OnInit {
         setTimeout(() => this.state = "input", 2000);
       },
       response => {
+        this.state = "error";
         //POST call in error
       },
       () => {
         //The POST observable is now completed
+        this.state = "success";
       });
   }
 
